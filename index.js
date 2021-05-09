edfc203fc6d75e15cd4febef6e27e6e1e1b76ec3
DrazzUwU <83860275+DrazzUwU@users.noreply.github.com>
2021-05-09 11:13:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -65,29 +65,29 @@ Client.on("message", message => {
             }
             else {
                 mention.roles.add("753257762145828935");
-                message.channel.send(mention.displayName + ("a fermé sa gueule :)"));
+                message.channel.send(mention.displayName + " a fermé sa gueule :)");
             }
         }
         else if (message.content.startsWith(prefix + "untg")){
             let mention = message.mentions.members.first();
 
             if(mention == undefined){
-                message.reply("Il faut mentionner une personne pour ne plus qu'elle se tg :^)");
+                message.reply(" Il faut mentionner une personne pour ne plus qu'elle se tg :^)");
             }
             else {
                 mention.roles.remove("753257762145828935");
-                message.channel.send(mention.displayName + ("ne ferme plus sa gueule :D, Gg."));
+                message.channel.send(mention.displayName + (" ne ferme plus sa gueule :D, Gg."));
             }
         }
     }
     //-help
     if(message.content.startsWith(prefix + "help")){
         if(message.member.hasPermission("ADMINISTRATOR")){
-            message.reply(", Voici la liste des commandes pour les __admins__ bgs ^^ : https://docs.google.com/document/d/1BRYxir2skidvbE53Sr3CZSO7PUIslAnI-EtQjQMtTx0/edit?usp=sharing ");
+            message.channel.send(mention.displayName + (", Voici la liste des commandes pour les __admins__ bgs ^^ : https://docs.google.com/document/d/1BRYxir2skidvbE53Sr3CZSO7PUIslAnI-EtQjQMtTx0/edit?usp=sharing "));
         }
-        else{
-            message.reply(", Voici la liste des commandes pour les __membres__ bgs ^^ : https://docs.google.com/document/d/1a6_a8bAiUe1bhgOhkGYhBvIx0LsP5DOhBvjCZnjxe-k/edit?usp=sharing")
-        } 
+        else {
+            message.channel.send(mention.displayName + (", Voici la liste des commandes pour les __membres__ bgs ^^ : https://docs.google.com/document/d/1a6_a8bAiUe1bhgOhkGYhBvIx0LsP5DOhBvjCZnjxe-k/edit?usp=sharing"));
+        }  
     }
         
 });
