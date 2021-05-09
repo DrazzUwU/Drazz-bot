8a715cc8299118cf636fafe8a00eb5078828bfd7
DrazzUwU <83860275+DrazzUwU@users.noreply.github.com>
2021-05-09 11:24:53 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -76,18 +76,18 @@ Client.on("message", message => {
             }
             else {
                 mention.roles.remove("753257762145828935");
-                message.channel.send(mention.displayName + (" ne ferme plus sa gueule :D, Gg."));
+                message.channel.send(mention.displayName + " ne ferme plus sa gueule :D, Gg.");
             }
         }
     }
     //-help
     if(message.content.startsWith(prefix + "help")){
         if(message.member.hasPermission("ADMINISTRATOR")){
-            message.channel.send(mention.displayName + (", Voici la liste des commandes pour les __admins__ bgs ^^ : https://docs.google.com/document/d/1BRYxir2skidvbE53Sr3CZSO7PUIslAnI-EtQjQMtTx0/edit?usp=sharing "));
+            message.reply(", Voici la liste des commandes pour les __admins__ bgs ^^ : https://docs.google.com/document/d/1BRYxir2skidvbE53Sr3CZSO7PUIslAnI-EtQjQMtTx0/edit?usp=sharing ");
         }
         else {
-            message.channel.send(mention.displayName + (", Voici la liste des commandes pour les __membres__ bgs ^^ : https://docs.google.com/document/d/1a6_a8bAiUe1bhgOhkGYhBvIx0LsP5DOhBvjCZnjxe-k/edit?usp=sharing"));
-        }  
+            message.reply( ", Voici la liste des commandes pour les __membres__ bgs ^^ : https://docs.google.com/document/d/1a6_a8bAiUe1bhgOhkGYhBvIx0LsP5DOhBvjCZnjxe-k/edit?usp=sharing");
+        }
     }
         
 });
