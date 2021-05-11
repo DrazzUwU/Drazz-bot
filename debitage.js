bb1175a0b52231c3f52dbcd2efa30de775bf8652
DrazzUwU <83860275+DrazzUwU@users.noreply.github.com>
2021-05-11 22:42:57 +0200

    Delete debitage.js

--- debitage.js
+++
@@ -1,9 +0,0 @@
-const { MessageMentions } = require("discord.js");
-
-if(message.content.startsWith(prefix + "debitage")){
-    message.reply(" debite ses morts à " + MessageMentions)
-
-    if(mention == undefined){
-        message.reply("Il faut mentionner une personne pour debiter :^)");
-    }
-}
